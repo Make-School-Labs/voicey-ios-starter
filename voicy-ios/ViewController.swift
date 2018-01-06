5fc4b61838689481e1ad397cabc1dbf9b4928406
Eliel Gordon <gordoneliel@gmail.com>
2018-01-05 20:45:30 -0800

    Initial Commit

---
+++ voicy-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voicy-ios
+//
+//  Created by Eliel A. Gordon on 1/5/18.
+//  Copyright © 2018 Eliel Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
