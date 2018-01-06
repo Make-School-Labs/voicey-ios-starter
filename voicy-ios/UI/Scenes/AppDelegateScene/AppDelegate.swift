1eb3006cfa374e8ac6773170d127a63f59c06b21
Eliel Gordon <gordoneliel@gmail.com>
2018-01-05 21:16:28 -0800

    Add boilerplate controllers for memo, authentication. Add extensions to UICollectionViews

---
+++ voicy-ios/UI/Scenes/AppDelegateScene/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  voicy-ios
+//
+//  Created by Eliel A. Gordon on 1/5/18.
+//  Copyright © 2018 Eliel Gordon. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ voicy-ios/UI/Scenes/MemoScene/ViewControllers/MemoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemoListViewController.swift
+//  voicy-ios
+//
+//  Created by Eliel A. Gordon on 1/5/18.
+//  Copyright © 2018 Eliel Gordon. All rights reserved.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
